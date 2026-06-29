18ad1a1a3740974e069f44a6178343c5ab691a13
zsz15964443357-lab <zsz15964443357@gmail.com>
2026-06-29 22:22:55 +0800

    chore: add visualization module boundary

---
+++ pirl_nav/visualization/__init__.py
@@ -0,0 +1,5 @@
+"""Visualization and review artifact boundary.
+
+Scenario previews, rollout plots, and risk visualizations should be introduced
+before large-scale training so scenario quality can be reviewed manually.
+"""
